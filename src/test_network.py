1f8bdd3a865c3bcaf21efdcb1a5e97de371e80d9
zhaoxm <xiaomengzhaopy@gmail.com>
2016-09-28 12:53:10 +0800

    first commit

---
+++ src/test_network.py
@@ -0,0 +1,12 @@
+'''
+Created on Sep 26, 2016
+
+@author: zhaoxm
+'''
+import mnist_loader
+import network
+
+if __name__ == '__main__':
+    training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+    net = network.Network([784, 30, 10])
+    net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
